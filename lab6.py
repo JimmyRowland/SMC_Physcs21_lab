c88db61571257ca6287250007edc1ae5de43f068
JimmyRowland <tian_yu@student.smc.edu>
2017-04-04 23:37:38 -0700

    lab7

--- lab6.py
+++ lab6.py
@@ -1,9 +1,10 @@
 g = 9.80665
-r=1
-r_delta=0.1
-t_experimental=[[2,2,2],[2,2,2]]
-t_max=[2,2]
-t_min=[1,1]
+#g=1
+r=16.59/100
+r_delta=0.05/100
+t_experimental=[[32.03,29.08,30.25],[2,2,2]]
+t_max=[32.03,2]
+t_min=[29.08,1]
 t=[]
 t_delta=[]
 pi=3.1415926
@@ -11,19 +12,21 @@ for i in range(len(t_experimental)):
     temp=0
     # print(sum(t_experimental[i])/len(t_experimental[i]))
     t.append(sum(t_experimental[i])/len(t_experimental[i]))
-    t_delta.append((t_max[i]-t_min[i])/2)
+    t_delta.append((t_max[i]-t_min[i])/2+0.02)
 n=50
-m_experimental=[[1],[2]]
-m_max=[2,2]
-m_min=[1,1]
-m=[]
+m_experimental=[8.4/g,2]
+m_max=[8.5/g,2]
+m_min=[8.3/g,1]
+m=[469.84/1000,1] #weight
 m_delta=[]
 for i in range(len(m_experimental)):
+    print(m_max[i],m_min[i],(m_max[i]+m_min[i])/2)
     m.append((m_max[i]+m_min[i])/2)
     m_delta.append((m_max[i]-m_min[i])/2)
 w=[]
 w_delta=[]
-for i in m:
+for i in m_experimental:
+    print('m',i,i*g)
     w.append(i*g)
 for i in m_delta:
     w_delta.append(i*g)
@@ -32,14 +35,15 @@ f_delta=[]
 
 for i in range(len(m_experimental)):
     f.append(4*pi**2*m[i]*r*n**2/t[i]**2)
-    f_delta.append(r_delta/r+2*t_delta[i]/t[i])
+    f_delta.append((r_delta/r+2*t_delta[i]/t[i])*f[i])
 print("t",t)
 for i in range(len(f)):
-    print(i+1,"f",f[i],"f_delta",f_delta[i],"f_min",(f[i]-f_delta[i])/2,"f_max",
-          (f[i]+f_delta[i])/2,)
-    print("w",w[i],"w_delta",w_delta[i],"w min",(w[i]-w_delta[i])/2,"w max",(w[i]+w_delta[i])/2)
+    print(i+1,"f",f[i],"f_delta",f_delta[i],"f_min",(f[i]-f_delta[i]),"f_max",
+          (f[i]+f_delta[i]),)
+    print("w",w[i],"w_delta",w_delta[i],"w min",(w[i]-w_delta[i]),"w max",(w[i]+w_delta[i]))
 # print("f",f,"f_delta",f_delta)
 # print("w",w,"w_delta",w_delta)
 
-
+print('m',m,m_delta)
+print("r",r)
 
---
+++ lab7.py
@@ -0,0 +1,81 @@
+#three significant digits
+g = 9.80665
+#part 1
+force_average=1.969
+integral_distance_force=2.62
+h_start=0.509
+h_end=1.09
+m_hanging=0.2
+
+
+
+h_delta=h_end-h_start
+work_done_by_average_force=h_delta*force_average
+comparison_workByForce_integral= work_done_by_average_force / integral_distance_force
+change_in_potential_energy=m_hanging*g*h_delta
+difference_work_potential=(work_done_by_average_force-change_in_potential_energy)/change_in_potential_energy
+print("work_done_by_average_force",work_done_by_average_force,"comparison_workByForce_integral",comparison_workByForce_integral,'change_in_potential_energy',change_in_potential_energy,'difference_work_potential',difference_work_potential)
+
+
+
+#part 2
+s_delta=[0.1,0.2,0.43]
+k_spring_constant=11.1
+integral_work_by_hand=[0.042,0.211,0.997]
+
+
+u_spring_potential=[]
+difference_work_spring_2=[]
+for i in range(len(s_delta)):
+    u_spring_potential.append(k_spring_constant*s_delta[i]**2/2)
+    difference_work_spring_2.append((integral_work_by_hand[i]-u_spring_potential[i])/u_spring_potential[i])
+print("u_spring_potential",u_spring_potential,"difference_work_spring_2",difference_work_spring_2)
+
+
+#part3
+
+
+
+m_cart=1
+m_sensor=1
+v_final_horizontal=1
+integral_push =1
+
+
+
+m_cart_sensor=m_cart+m_sensor
+k_delta=v_final_horizontal**2*m_cart_sensor/2
+difference_work_kDelta=integral_push/k_delta
+print("k_delta",k_delta,"difference_work_kDelta",difference_work_kDelta)
+
+
+#part 4
+t_cart_delta=0.5
+w_cart=1
+position=[0,0.054,0.108,0.216]
+velocity=[0.243,0.291,0.330,0.387]
+a_average=0.253
+
+
+h_vertical=[]
+potential_h=[]
+k_cart=[]
+e_mechanical_total=[]
+difference_points_total=[]
+sin_O=a_average/g
+
+for i in range(len(position)):
+    h_vertical.append(sin_O*position[i]*(-1))
+
+for i in range(len(position)):
+        h_vertical[i] -= h_vertical[3]
+        potential_h.append(h_vertical[i] * m_cart * g)
+        k_cart.append(velocity[i] ** 2 * m_cart / 2)
+        e_mechanical_total.append(k_cart[i] + potential_h[i])
+for i in range(len(position)):
+    difference_points_total.append((e_mechanical_total[-3+i]-e_mechanical_total[-4+i])/e_mechanical_total[-3+i])
+    print(e_mechanical_total[-3 + i], e_mechanical_total[-4 + i])
+print('sin_O',sin_O)
+print('h_vertical',h_vertical,'potential_h',potential_h)
+print('k_cart',k_cart,'e_mechanical_total',e_mechanical_total)
+print('difference_points_total',difference_points_total)
